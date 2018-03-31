3971408cad07cdcbc80a7be1753b4f18216e2a04
AmallTian <artem.exception@gmail.com>
2018-03-31 21:17:13 +0400

    Добавил коменты к тестам

--- src/test/java/ru/education/calculator/CalculatorTest.java
+++ src/test/java/ru/education/calculator/CalculatorTest.java
@@ -5,39 +5,43 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
-
+    // сложение
     @Test
     public void add() {
         Calculator calculator = new Calculator();
-        calculator.add(1,1);
+        calculator.add(1, 1);
         assertEquals(2, calculator.getResult());
     }
 
+    //вычитание
     @Test
     public void diff() {
         Calculator calculator = new Calculator();
-        calculator.diff(4,2);
+        calculator.diff(4, 2);
         assertEquals(2, calculator.getResult());
     }
 
+    //умножение
     @Test
     public void multi() {
         Calculator calculator = new Calculator();
-        calculator.multi(2,2);
+        calculator.multi(2, 2);
         assertEquals(4, calculator.getResult());
     }
 
+    //деление
     @Test
     public void div() {
         Calculator calculator = new Calculator();
-        calculator.div(4,2);
+        calculator.div(4, 2);
         assertEquals(2, calculator.getResult());
     }
 
+    //возведение в степень
     @Test
     public void deg() {
         Calculator calculator = new Calculator();
-        calculator.deg(2,3);
+        calculator.deg(2, 3);
         assertEquals(8, calculator.getResult());
     }
 }
